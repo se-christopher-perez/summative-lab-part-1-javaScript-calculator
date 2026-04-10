0144cae3ad7ac45f2fb5d384e01dc97aca5207f7
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-04-09 20:11:38 -0400

    Added functions for + - * /

---
+++ index.js
@@ -0,0 +1,27 @@
+
+function addNumbers (num1, num2) {
+    return num1 + num2
+}
+
+
+function subtractNumbers (num1, num2) {
+    return num1 - num2
+}
+
+
+function multiplyNumbers (num1, num2) {
+    return num1 * num2
+}
+
+
+function divideNumbers (num1, num2) {
+    return num1 / num2
+}
+
+console.log(addNumbers(100, 10))
+
+console.log(subtractNumbers(100, 10))
+
+console.log(multiplyNumbers(100,10))
+
+console.log(divideNumbers(100, 10))
